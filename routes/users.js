7069f3e1358b73e691f7341dfa32d22b119211d5
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-02-01 15:39:47 +0530

    Add unique id generator, and use it for users

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const file = require('../lib/file');
 const readBody = require('../lib/read-body');
+const generateId = require('../lib/generate-id');
 const dataPath = './fixtures/users.json';
 
 const getUser = async(req, res) => {
     const users = await file.read(dataPath);
-    const  user = users.find(user => user.id == req.params.id);
+    const  user = users.find(user => user.id === req.params.id);
     res.send(user);
 };
 
@@ -16,17 +17,17 @@ const getUsers = async(req, res) => {
 
 const createUser = async(req, res) => {
     const users = await file.read(dataPath);
-    const id = users.length + 1;
+    const id = generateId();
     const body = await readBody(req);
-    const newUser = { ...JSON.parse(body), id };
+    const newUser = { id, ...JSON.parse(body) };
     await file.write(JSON.stringify([ ...users, newUser], null, 2), dataPath);
     res.status(201).send(newUser);
 }
 
 const updateUser = async(req, res) => {
     const users = await file.read(dataPath);
-    const user = users.find(user => user.id == req.params.id);
-    const updUsers = users.filter(user => user.id != req.params.id);
+    const user = users.find(user => user.id === req.params.id);
+    const updUsers = users.filter(user => user.id !== req.params.id);
     const body = await readBody(req);
     const updUser = { ...user, ...JSON.parse(body), id: user.id };
     await file.write(JSON.stringify([...updUsers, updUser], null, 2), dataPath);
@@ -35,7 +36,7 @@ const updateUser = async(req, res) => {
 
 const deleteUser = async(req, res) => {
     const users = await file.read(dataPath);
-    const updUsers = users.filter(user => user.id != req.params.id);
+    const updUsers = users.filter(user => user.id !== req.params.id);
     await file.write(JSON.stringify(updUsers, null, 2), dataPath);
     res.sendStatus(204);
 }
