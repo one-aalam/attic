62ad3223930b8303b1641aded994111590acbed6
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-02-01 16:25:24 +0530

    Generate exception when an item not found

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,16 @@ const router = require('express').Router();
 const file = require('../lib/file');
 const jsonBodyParser = require('../lib/json-body-parser');
 const generateId = require('../lib/generate-id');
+const NotFound = require('../lib/not-found-err');
+const NotFounder = require('../lib/not-found-handler');
 const dataPath = './fixtures/users.json';
 
 const getUser = async(req, res) => {
     const users = await file.read(dataPath);
     const  user = users.find(user => user.id === req.params.id);
+    if (!user) {
+        throw new NotFound();
+    }
     res.send(user);
 };
 
@@ -26,6 +31,9 @@ const createUser = async(req, res) => {
 const updateUser = async(req, res) => {
     const users = await file.read(dataPath);
     const user = users.find(user => user.id === req.params.id);
+    if (!user) {
+        throw new NotFound();
+    }
     const updUsers = users.filter(user => user.id !== req.params.id);
     const updUser = { ...user, ...req.body, id: user.id };
     await file.write(JSON.stringify([...updUsers, updUser], null, 2), dataPath);
@@ -34,6 +42,10 @@ const updateUser = async(req, res) => {
 
 const deleteUser = async(req, res) => {
     const users = await file.read(dataPath);
+    const user = users.find(user => user.id === req.params.id);
+    if (!user) {
+        throw new NotFound();
+    }
     const updUsers = users.filter(user => user.id !== req.params.id);
     await file.write(JSON.stringify(updUsers, null, 2), dataPath);
     res.sendStatus(204);
@@ -44,9 +56,9 @@ const userRoutes = (app) => {
     router.get('/', getUsers); // GET ALL
     router.post('/', jsonBodyParser, createUser); // CREATE
 
-    router.get('/:id', getUser); // GET
-    router.patch('/:id', jsonBodyParser, updateUser); // UPDATE
-    router.delete('/:id', deleteUser); // DELETE
+    router.get('/:id', getUser, NotFounder); // GET
+    router.patch('/:id', jsonBodyParser, updateUser, NotFounder ); // UPDATE
+    router.delete('/:id', deleteUser, NotFounder); // DELETE
 
     app.use('/users', router);
 };
