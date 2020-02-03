bb0c773f85227c76619bbe4463304087d5ac8ccc
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-02-03 19:41:48 +0530

    resolve paths

--- src/lib/find-users.ts
+++ src/lib/find-users.ts
@@ -1,25 +1,18 @@
 import path from 'path';
-const file = require('./file');
+import { readFile } from './file';
 
 import { IUser } from '../interfaces/';
 
 const dataPath = path.join(__dirname, '../fixtures/users.json');
 
 export const byNameAndPassword  = async ({ username, password }: IUser) => {
-    const users = await file.read(dataPath);
+    const users: any = await readFile(dataPath);
     return users.find((user: IUser) => user.name === username && user.password === password);
 }
 
 export const byUserId  = async ({ id }: IUser) => {
-    const users = await file.read(dataPath);
+    const users: any = await readFile(dataPath);
     return users.find((user: IUser) => user.id === id);
 }
 
 export const byUserToken = byUserId;
-
-// export const findUserById as byUserToken;
-
-
-// exports.byNameAndPassword = findUserByNameAndPassword;
-// exports.byUserId = findUserById;
-// exports.byUserToken = findUserById;
