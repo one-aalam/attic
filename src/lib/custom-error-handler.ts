6d48980b0bc964c87a7caec377d1255acc0e1448
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-02-03 23:59:57 +0530

    Add database support: PostgresSQL + TypeORM

--- src/lib/custom-error-handler.ts
+++ src/lib/custom-error-handler.ts
@@ -2,20 +2,30 @@ import { ErrorRequestHandler, Response, NextFunction, Errback } from 'express';
 
 import { UserNotAuthorizedError, NotFoundError } from './custom-error';
 
-export const notAuthorizedHandler: ErrorRequestHandler = (err: Errback, _, res: Response, next: NextFunction) => {
-    if (err instanceof UserNotAuthorizedError) {
-        res.sendStatus(403);
-        next();
-    } else {
-        next(err);
-    }
-}
+export const notAuthorizedHandler: ErrorRequestHandler = (
+  err: Errback,
+  _,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (err instanceof UserNotAuthorizedError) {
+    res.sendStatus(403);
+    next();
+  } else {
+    next(err);
+  }
+};
 
-export const notFoundHandler: ErrorRequestHandler = (err: Errback, _, res: Response, next: NextFunction) => {
-    if (err instanceof NotFoundError) {
-        res.status(404).send(``);
-        next();
-    } else {
-        next(err);
-    }
-}
+export const notFoundHandler: ErrorRequestHandler = (
+  err: Errback,
+  _,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (err instanceof NotFoundError) {
+    res.status(404).send(``);
+    next();
+  } else {
+    next(err);
+  }
+};
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,24 +1,9 @@
 import express, { Request, Response, Express, Router } from 'express';
 import path from 'path';
 import bodyParse from 'body-parser';
-import multer, {  } from 'multer';
+import multer from 'multer';
 
 import { IUser, IUserAndAuthFnRequest } from 'interfaces';
-
-const router: Router = express.Router();
-
-const bodyParser = bodyParse.json({
-    limit: '100kb'
-});
-const storage = multer.diskStorage({
-    destination: function (_, __, cb) {
-      cb(null, path.join(__dirname, '../../uploads'))
-    },
-    filename: function (_, file, cb) {
-      cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
-    }
-});
-const upload = multer({ storage });
 
 import { requireAuth } from 'lib/require-auth';
 import { enforce } from 'lib/enforce';
@@ -27,73 +12,101 @@ import { generateId } from 'lib/generate-id';
 import { NotFoundError } from 'lib/custom-error';
 import { notFoundHandler, notAuthorizedHandler } from 'lib/custom-error-handler';
 
+const router: Router = express.Router();
+
+const bodyParser = bodyParse.json({
+  limit: '100kb',
+});
+const storage = multer.diskStorage({
+  destination(_, __, cb) {
+    cb(null, path.join(__dirname, '../../uploads'));
+  },
+  filename(_, _file, cb) {
+    cb(null, Date.now() + path.extname(_file.originalname)); // Appending extension
+  },
+});
+const upload = multer({ storage });
+
 const dataPath = path.join(__dirname, '../fixtures/users.json');
 
-const getUser = async(req: Request, res: Response) => {
-    const users:any = await file.readFile(dataPath);
-    const  user = users.find((user: IUser) => user.id === req.params.id);
-    if (!user) {
-        throw new NotFoundError();
-    }
-    res.send(user);
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const getUser = async (req: Request, res: Response) => {
+  const users: any = await file.readFile(dataPath);
+  const user = users.find((user: IUser) => user.id === req.params.id);
+  if (!user) {
+    throw new NotFoundError();
+  }
+  res.send(user);
 };
 
-const getUsers = async(_: Request, res: Response) => {
-    const users:any = await file.readFile(dataPath);
-    res.send(users);
+const getUsers = async (_: Request, res: Response) => {
+  const users: any = await file.readFile(dataPath);
+  res.send(users);
 };
 
-const createUser = async(req: Request, res: Response) => {
-    const users: any = await file.readFile(dataPath);
-    const id = generateId();
-    // @ts-ignore: Uncallable code error
-    const avatar = (req.files || []).map((file: Express.Multer.File) => `/uploads/${file.filename}`);
-    const newUser = { id, ...req.body, avatar };
-    await file.writeFile(JSON.stringify([ ...users, newUser], null, 2), dataPath);
-    res.status(201).send(newUser);
-}
+const createUser = async (req: Request, res: Response) => {
+  const users: any = await file.readFile(dataPath);
+  const id = generateId();
+  // @ts-ignore: Uncallable code error
+  const avatar = (req.files || []).map((file: Express.Multer.File) => `/uploads/${file.filename}`);
+  const newUser = { id, ...req.body, avatar };
+  await file.writeFile(JSON.stringify([...users, newUser], null, 2), dataPath);
+  res.status(201).send(newUser);
+};
 
-const updateUser = async(req: IUserAndAuthFnRequest, res: Response) => {
-    const users: any = await file.readFile(dataPath);
-    const user = users.find((user: IUser) => user.id === req.params.id);
-    req.authorize!(user);
-    if (!user) {
-        throw new NotFoundError();
-    }
-    // @ts-ignore: Uncallable code error
-    const avatar = (req.files || []).map((file: Express.Multer.File) => `/uploads/${file.filename}`);
-    const updUsers = users.filter((user: IUser) => user.id !== req.params.id);
-    const updUser = { ...user, ...req.body, id: user.id, avatar };
-    await file.writeFile(JSON.stringify([...updUsers, updUser], null, 2), dataPath);
-    res.status(200).send(updUser);
-}
+const updateUser = async (req: IUserAndAuthFnRequest, res: Response) => {
+  const users: any = await file.readFile(dataPath);
+  const user = users.find((user: IUser) => user.id === req.params.id);
+  req.authorize!(user);
+  if (!user) {
+    throw new NotFoundError();
+  }
+  // @ts-ignore: Uncallable code error
+  const avatar = (req.files || []).map((file: Express.Multer.File) => `/uploads/${file.filename}`);
+  const updUsers = users.filter((user: IUser) => user.id !== req.params.id);
+  const updUser = { ...user, ...req.body, id: user.id, avatar };
+  await file.writeFile(JSON.stringify([...updUsers, updUser], null, 2), dataPath);
+  res.status(200).send(updUser);
+};
 
-const deleteUser = async(req: IUserAndAuthFnRequest, res: Response) => {
-    const users: any = await file.readFile(dataPath);
-    const user = users.find((user: IUser) => user.id === req.params.id);
-    req.authorize!(user);
-    if (!user) {
-        throw new NotFoundError();
-    }
-    const updUsers = users.filter((user: IUser) => user.id !== req.params.id);
-    await file.writeFile(JSON.stringify(updUsers, null, 2), dataPath);
-    res.sendStatus(204);
-}
-
+const deleteUser = async (req: IUserAndAuthFnRequest, res: Response) => {
+  const users: any = await file.readFile(dataPath);
+  const user = users.find((user: IUser) => user.id === req.params.id);
+  req.authorize!(user);
+  if (!user) {
+    throw new NotFoundError();
+  }
+  const updUsers = users.filter((user: IUser) => user.id !== req.params.id);
+  await file.writeFile(JSON.stringify(updUsers, null, 2), dataPath);
+  res.sendStatus(204);
+};
 
 const updateUserPolicy = (user: IUser, resource: IUser) => user.id === resource.id;
 const deleteUserPolicy = updateUserPolicy;
 
-
 export const userRoutes = (app: Express) => {
-    router.use(requireAuth);
+  router.use(requireAuth);
 
-    router.get('/', getUsers); // GET ALL
-    router.post('/', bodyParser, upload.array('avatar'), createUser); // CREATE
+  router.get('/', getUsers); // GET ALL
+  router.post('/', bodyParser, upload.array('avatar'), createUser); // CREATE
 
-    router.get('/:id', getUser, notFoundHandler); // GET
-    router.patch('/:id', enforce(updateUserPolicy), bodyParser, upload.array('avatar'), updateUser, notAuthorizedHandler, notFoundHandler ); // UPDATE
-    router.delete('/:id', enforce(deleteUserPolicy), deleteUser, notAuthorizedHandler, notFoundHandler); // DELETE
+  router.get('/:id', getUser, notFoundHandler); // GET
+  router.patch(
+    '/:id',
+    enforce(updateUserPolicy),
+    bodyParser,
+    upload.array('avatar'),
+    updateUser,
+    notAuthorizedHandler,
+    notFoundHandler,
+  ); // UPDATE
+  router.delete(
+    '/:id',
+    enforce(deleteUserPolicy),
+    deleteUser,
+    notAuthorizedHandler,
+    notFoundHandler,
+  ); // DELETE
 
-    app.use('/users', router);
+  app.use('/users', router);
 };
