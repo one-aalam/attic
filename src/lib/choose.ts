83652886c0b2437b73575ee4a7ac72522fd82384
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-02-04 12:17:33 +0530

    Re-organize `omit` and `pick` methods

--- src/lib/choose.ts
+++ src/lib/choose.ts
@@ -9,3 +9,13 @@ export const omit = (obj: O, keys: string[]) => {
     return target;
   }, {});
 };
+
+export const pick = (obj: O, keys: string[]) => {
+  return Object.keys(obj).reduce((target: O, key) => {
+    if (keys.includes(key)) {
+      // eslint-disable-next-line no-param-reassign
+      target[key] = obj[key];
+    }
+    return target;
+  }, {});
+};
